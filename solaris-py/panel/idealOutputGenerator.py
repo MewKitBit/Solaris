9fe7a66d8c79de238086fdbd2003d8e3c42eb859
MewBiscuit <91554425+MewBiscuit@users.noreply.github.com>
2025-11-24 18:47:45 +0100

    Separate steps of ideal generator into functions

--- solaris-py/panel/idealOutputGenerator.py
+++ solaris-py/panel/idealOutputGenerator.py
@@ -25,24 +25,12 @@ class IdealOutputGenerator :
         self.temp_params = TEMPERATURE_MODEL_PARAMETERS['sapm'][temp_model]
         # TODO: Could include albedo in the future for more granular control. For now left at default PVLib 0.25
         self.albedo = 0.25
+        self.irradiance = None
+        self.temperature = None
+        self.ideal_power = None
 
-    def calculate_power(self, weather: DataFrame, solpos: DataFrame, output_file: str):
-        """
-        Runs the core physics using the provided weather and solpos dataframes.
-
-        Args:
-            weather (Series): Weather data series.
-            solpos (Series): Solar position data series.
-            output_file (str): Name/path of file where output will be written.
-        """
-        if weather.len() != solpos.len():
-            raise ValueError("Weather length does not match solar position data.")
-
-        if not output_file.endswith(".csv"):
-            output_file += ".csv"
-
-        # Get irradiance on the panel
-        poa = irradiance.get_total_irradiance(
+    def calculate_irradiance(self, weather, solpos):
+        self.irradiance = irradiance.get_total_irradiance(
             surface_tilt=self.mount.surface_tilt,
             surface_azimuth=self.mount.surface_azimuth,
             solar_zenith=solpos['apparent_zenith'],
@@ -53,24 +41,45 @@ class IdealOutputGenerator :
             albedo=self.albedo
         )
 
-        # Get cell temperature
-        cell_temp = temperature.sapm_cell(
-            poa_global=poa['poa_global'],
+    def calculate_temperature(self, weather):
+        self.temperature = temperature.sapm_cell(
+            poa_global=self.irradiance['poa_global'],
             temp_air=weather['temp_air'],
             wind_speed=weather['wind_speed'],
             **self.temp_params  # Unpack the temp model dict
         )
 
-        # Calculate ideal DC power
-        ideal_power = pvsystem.pvwatts_dc(
-            effective_irradiance=poa['poa_global'],
-            temp_cell=cell_temp,
+    def calculate_power(self):
+        self.ideal_power = pvsystem.pvwatts_dc(
+            effective_irradiance=self.irradiance['poa_global'],
+            temp_cell=self.temperature,
             pdc0=self.module_params['pdc0'],
             gamma_pdc=self.module_params['gamma_pdc']
         )
+    def generate_output(self, weather: DataFrame, solpos: DataFrame, output_file: str):
+        """
+        Runs the core physics using the provided weather and solpos dataframes.
+
+        Args:
+            weather (Series): Weather data series.
+            solpos (Series): Solar position data series.
+            output_file (str): Name/path of file where output will be written.
+        """
+        if weather.len() != solpos.len():
+            raise ValueError("Weather length does not match solar position data length.")
+
+        if not output_file.endswith(".csv"):
+            output_file += ".csv"
+
+        # Get irradiance on the panel
+        self.calculate_irradiance(weather, solpos)
+        # Get cell temperature
+        self.calculate_temperature(weather)
+        # Calculate ideal DC power
+        self.calculate_power()
 
         # Save to file
-        ideal_power.to_csv(
+        self.ideal_power.to_csv(
             output_file,
             header=True,
         )
